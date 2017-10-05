c8af2c3c7d79401062a0503d712318c20b949327
Zhang <zhangjunpeng9354@126.com>
2017-10-06 10:52:14 +1100

    Normalized the name rules.

--- alexnet_cifar10/input.py
+++ alexnet_cifar10/input.py
@@ -12,7 +12,7 @@ NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = 50000
 DEFALFT_DATASET_DIR = '/datasets/cifar-10-batches-py'
 
 
-def load_cifar10(path=DEFALFT_DATASET_DIR):
+def _load_cifar10(path=DEFALFT_DATASET_DIR):
     '''Read training and testing samples from CIFAR-10 python batch files.
 
     Recommendation: AlexNet in the tutorials provided by TensorFlow describes
@@ -26,16 +26,13 @@ def load_cifar10(path=DEFALFT_DATASET_DIR):
     then path should be '/tmp/CIFAR-10/cifar-10-batches-py'.
     Please note that the dataset should be downloaded in advance.
     :return:
-    train_images: in raw format, [None, 3024], dtype = np.float32.
-    train_labels: in one-hot format, dtype = np.int32.
+    train_images: raw data in numpy.ndarray format, [None, 3024], dtype = numpy.float32.
+    train_labels: one-hot lables in format, numpy.ndarray, [None, NUM_CLASSES], dtype = numpy.int32.
     test_images: the same as train_images.
     test_labels: the same as train_labels.
     '''
-
     _training_file_names = [os.path.join(path, 'data_batch_%d' % i) for i in xrange(1, 6)]
     _test_file_names = os.path.join(path, 'test_batch')
-
-
 
     import cPickle
 
@@ -45,17 +42,23 @@ def load_cifar10(path=DEFALFT_DATASET_DIR):
             _train_batches.append(cPickle.load(_training_file))
 
     with open(_test_file_names, 'rb') as f:
-        test_batch = cPickle.load(f)
+        _test_batch = cPickle.load(f)
 
-    train_images = np.vstack(batch['data'] for batch in _train_batches)
-    train_ys = np.hstack(batch['labels'] for batch in _train_batches)
+    train_images = np.vstack(_training_batch['data'] for _training_batch in _train_batches).astype(np.float32)
+    test_images = _test_batch['data'].astype(np.float32)
 
-    test_images = test_batch['data']
-    test_ys = np.array(test_batch['labels'])
+    _train_ys = np.hstack(batch['labels'] for batch in _train_batches)
+    _test_ys = np.array(_test_batch['labels'])
 
-    train_labels = np.zeros(shape=(len(train_ys), 10), dtype=np.float32)
-    train_labels[np.arange(len(train_ys)), train_ys] = 1
-    test_labels = np.zeros(shape=(len(test_ys), 10), dtype=np.float32)
-    test_labels[np.arange(len(test_ys)), test_ys] = 1
+    train_labels = np.zeros(shape=(len(_train_ys), 10), dtype=np.int32)
+    train_labels[np.arange(len(_train_ys)), _train_ys] = 1
+    test_labels = np.zeros(shape=(len(_test_ys), 10), dtype=np.int32)
+    test_labels[np.arange(len(_test_ys)), _test_ys] = 1
 
-    return train_images, train_labels, test_images, test_labels
+    return train_images, train_labels, \
+           test_images, test_labels
+
+
+def create_batch_dataset(path=DEFALFT_DATASET_DIR):
+    _training_images, _training_labels, _test_images, _test_labels = _load_cifar10(path=path)
+    return None
